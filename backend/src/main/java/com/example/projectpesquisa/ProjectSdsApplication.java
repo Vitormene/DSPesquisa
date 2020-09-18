22054959d89209ca30440de8c7e2cc0d37f2e2da
Vitor Menezes <Menezesal@outlook.com>
2020-09-17 21:21:33 -0300

    Project created

---
+++ backend/src/main/java/com/example/projectpesquisa/ProjectSdsApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectpesquisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSdsApplication.class, args);
+	}
+
+}
