9a1178c63bdad0f8fcd8cf50e496135217ad1294
Vitor Menezes <Menezesal@outlook.com>
2020-09-21 22:50:11 -0300

    DTO, Service

--- backend/src/main/java/com/example/projectpesquisa/repositories/GameRepository.java
+++ backend/src/main/java/com/example/projectpesquisa/repositories/GameRepository.java
@@ -1,9 +1,11 @@
 package com.example.projectpesquisa.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import com.example.projectpesquisa.entities.Game;
-
+@Repository
 public interface GameRepository extends JpaRepository<Game, Long> {
 
 }
