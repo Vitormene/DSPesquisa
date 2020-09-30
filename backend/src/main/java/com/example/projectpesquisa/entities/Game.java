62a3146fd805ee32202e257a6d2c7361a652a31f
Vitor Menezes <Menezesal@outlook.com>
2020-09-29 22:19:42 -0300

    POST records

--- backend/src/main/java/com/example/projectpesquisa/entities/Game.java
+++ backend/src/main/java/com/example/projectpesquisa/entities/Game.java
@@ -14,6 +14,7 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.example.projectpesquisa.entities.enums.Platform;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_game")
@@ -28,7 +29,7 @@ public class Game implements Serializable {
 	private Long id;
 	private String title;
 	private Platform platform;
-
+	
 	@ManyToOne
 	@JoinColumn(name = "genre_id")
 	private Genre genre;
